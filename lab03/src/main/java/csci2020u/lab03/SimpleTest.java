fd116517ca7ad39ca1beb3db1070890fbced218c
AdamGreen <adam.green3@ontariotechu.net>
2020-01-29 15:10:59 -0500

    Final lab3 commit

---
+++ lab03/src/main/java/csci2020u/lab03/SimpleTest.java
@@ -0,0 +1,16 @@
+package csci2020u.lab02;
+
+import java.io.*;
+import java.util.*;
+
+public class SimpleTest{
+
+	public static void main(String[] args){
+
+		/*Print Hello World*/
+
+		System.out.println("Hello World!");
+
+	}
+
+}
